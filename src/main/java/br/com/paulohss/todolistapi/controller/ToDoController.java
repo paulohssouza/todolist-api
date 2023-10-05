2c35822be6c2367116f9527b2a9c049f0403441a
Paulo Henrique <paulohss.souza@gmail.com>
2023-10-05 12:11:49 -0300

    Creation and configuration of the controller that will bridge the gap between the frontend and the backend.

---
+++ src/main/java/br/com/paulohss/todolistapi/controller/ToDoController.java
@@ -0,0 +1,38 @@
+package br.com.paulohss.todolistapi.controller;
+
+import br.com.paulohss.todolistapi.entity.ToDo;
+import br.com.paulohss.todolistapi.service.ToDoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class ToDoController {
+
+    private ToDoService toDoService;
+
+    public ToDoController(ToDoService toDoService) {
+        this.toDoService = toDoService;
+    }
+
+    @PostMapping
+    List<ToDo> create(@RequestBody ToDo toDo) {
+        return toDoService.create(toDo);
+    }
+
+    @GetMapping
+    List<ToDo> list() {
+        return toDoService.list();
+    }
+
+    @PutMapping
+    List<ToDo> update(@RequestBody ToDo toDo) {
+        return toDoService.update(toDo);
+    }
+
+    @DeleteMapping("{id}")
+    List<ToDo> delete(@PathVariable("id") Long id) {
+        return toDoService.delete(id);
+    }
+}
